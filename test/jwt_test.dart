c6eb29db1b56cce73ce09652b10e84380fd72e9e
Christoph Menzel <christoph.menzel@methodpark.de>
2021-10-12 15:48:00 +0200

    Implement function to create a jwt

---
+++ test/jwt_test.dart
@@ -0,0 +1,16 @@
+import 'package:app_store_connect/src/jwt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('JWT:', () {
+    test(
+        'createExpirationTimeAsUnixEpochTime - default call - returns expiration time as unix epoch time',
+        () {
+      var dateTime = DateTime.parse('2021-08-20 15:00:00Z');
+
+      var result = createExpirationTimeAsUnixEpochTime(dateTime);
+
+      expect(result, equals(1629472500));
+    });
+  });
+}
