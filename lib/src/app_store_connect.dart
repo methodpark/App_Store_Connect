8c8a1f4b187424673b94d4640e8ec085fe8e405f
Christoph Menzel <christoph.menzel@methodpark.de>
2021-10-12 18:59:57 +0200

    Extract profile handling into separate file

--- lib/src/app_store_connect.dart
+++ lib/src/app_store_connect.dart
@@ -1,11 +1,7 @@
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:app_store_connect_apis/src/jwt.dart';
-import 'package:http/http.dart' as http;
+import 'package:app_store_connect_apis/src/provisioning.dart';
 
-import 'constants.dart';
-import 'http_client.dart';
 import 'models/profile.dart';
 
 class AppStoreConnect {
@@ -13,6 +9,7 @@ class AppStoreConnect {
   final String _keyId;
   final String _keyFilePath;
   late final String _keyFileContent;
+  late final Provisioning _provisioning;
 
   /// [_issuer] Your issuer ID from the API keys page in App Store Connect
   ///
@@ -21,48 +18,17 @@ class AppStoreConnect {
   /// [_keyFilePath] Path to the private key file from App Store Connect
   AppStoreConnect(this._issuer, this._keyId, this._keyFilePath) {
     loadKeyFileContent();
+    _provisioning = Provisioning(_issuer, _keyId, _keyFileContent);
   }
 
   /// Gets a list of all provisioning profiles
   Future<List<Profile>> getAllProfiles() async {
-    var jwt = createJWT(_issuer, _keyFileContent, _keyId);
-
-    var response = await get(profilesUri, jwt);
-
-    var profiles = _convertResponseToListOfProfiles(response);
-
-    return profiles;
+    return _provisioning.getAllProfiles();
   }
 
   /// Downloads a specific provisioning profile which is defined by the [profileId]
   Future<Profile> downloadProfile(String profileId) async {
-    var jwt = createJWT(_issuer, _keyFileContent, _keyId);
-
-    var response = await get(profilesUri + '/' + profileId, jwt);
-
-    var profile = _convertResponseToProfile(response);
-
-    return profile;
-  }
-
-  List<Profile> _convertResponseToListOfProfiles(http.Response response) {
-    dynamic jsonResponse = jsonDecode(response.body);
-    List<dynamic> allProfiles = jsonResponse['data'] as List<dynamic>;
-
-    var profiles = allProfiles
-        .map((dynamic e) => Profile.fromJson(e as Map<String, dynamic>))
-        .toList();
-
-    return profiles;
-  }
-
-  Profile _convertResponseToProfile(http.Response response) {
-    dynamic jsonResponse = jsonDecode(response.body);
-
-    var profile =
-        Profile.fromJson(jsonResponse['data'] as Map<String, dynamic>);
-
-    return profile;
+    return _provisioning.downloadProfile(profileId);
   }
 
   void loadKeyFileContent() {
